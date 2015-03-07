1d200259f907dca66526edf663c30ee2d280e808
Quinn Johnson <quinn@graphistry.com>
2015-03-07 12:21:29 -0800

    Made dummy fin data

--- peopleMoney/public/javascripts/financialMap.js
+++ peopleMoney/public/javascripts/financialMap.js
@@ -3,8 +3,102 @@
 var d3          = require('d3');
 var request     = require('request');
 
+// Globals! Because hackathon!
+var svg;
+
+// Fake financial data. Should be done with call to server.
+// TODO: Get real data from REST calls.
+var fakeData = {
+
+    // Should have investments?
+    accounts: [
+        {
+            name: 'Account 1',
+            type: 'savings',
+            balance: 1000
+        }
+    ],
+
+    loans: [
+        {
+            name: 'Loan 1',
+            type: 'loan',
+            interestRate: 0.05,
+            balance: 2000
+        },
+        {
+            name: 'Loan 2',
+            type: 'mortgage',
+            interestRate: 0.035,
+            balance: 100000
+        },
+    ],
+
+    transactions: [
+        {
+            name: 'paycheck',
+            type: 'income',
+            amount: 2000
+        },
+        {
+            name: 'paycheck',
+            type: 'income',
+            amount: 2000
+        },
+        {
+            name: 'name1',
+            type: 'expense',
+            category: 1,
+            amount: 2000
+        },
+        {
+            name: 'name2',
+            type: 'expense',
+            category: 1,
+            amount: 2000
+        },
+        {
+            name: 'name2',
+            type: 'expense',
+            category: 1,
+            amount: 2000
+        },
+        {
+            name: 'name2',
+            type: 'expense',
+            category: 1,
+            amount: 2000
+        },
+        {
+            name: 'name3',
+            type: 'expense',
+            category: 2,
+            amount: 2000
+        },
+        {
+            name: 'name4',
+            type: 'expense',
+            category: 2,
+            amount: 2000
+        },
+        {
+            name: 'name5',
+            type: 'expense',
+            category: 2,
+            amount: 2000
+        },
+        {
+            name: 'name6',
+            type: 'expense',
+            category: 3,
+            amount: 2000
+        }
+    ]
+};
+
+
 function makeSvg() {
-    var svg = d3.select("#map-container").append("svg");
+    svg = d3.select("#map-container").append("svg");
         // .attr("width", width)
         // .attr("height", height)
 }
@@ -24,13 +118,6 @@ function getData(uid, cb) {
 function init () {
     console.log('Initializing Financial Map');
     makeSvg();
-    getData('ID', function (err, data) {
-        if (err) {
-            console.log('Error: ', err);
-            return;
-        }
-        console.log('Got Data: ', data);
-    });
 }
 
 
