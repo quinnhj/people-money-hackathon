914a6f694e8cf3f3d64f8fc683eda844785d49d5
David Wu <dwu7401@gmail.com>
2015-03-07 22:45:03 -0800

    health api calculates most $ values

--- peopleMoney/src/healthApi.js
+++ peopleMoney/src/healthApi.js
@@ -3,6 +3,12 @@
 var capitalApi = require('./capitalApi.js');
 var _ = require('underscore');
 
+///////////////////////////////////////////////////////////////////////
+// Helper functions used to calculate financial health of individual
+//
+// Note: All amounts (except ratios) are returned in centocents
+///////////////////////////////////////////////////////////////////////
+
 // Returns Debt / Balance as a float
 function getDebtBalanceRatio(uid, authToken, cb) {
     capitalApi.getAccounts(uid, authToken, function(err, val) {
@@ -68,20 +74,46 @@ function getDeposits(uid, authToken, startDate, endDate, cb) {
                 total += amount;
             }
         });
-        cb(err, total)
+        cb(err, total);
     }
     capitalApi.getAllTransactions(uid, authToken, f, [startDate, endDate]);
-
 }
-//total debt
-//total balance
-//income
-//expenses
 
+// Returns absolute value of total expenses across date range. Use Date.parse() for start/end dates
+// Ex. healthApi.getExpenses(1234, abc, Date.parse(Date(2015, 1, 1, 0, 0, 0)), Date.now(), cb);
+function getExpenses(uid, authToken, startDate, endDate, cb) {
+    var f = function(err, transactions) {
+        var total = 0.0;
+        _.each(transactions, function(el) {
+            var amount = el.amount;
+            if(amount < 0.0) {
+                total += amount;
+            }
+        });
+        cb(err, Math.abs(total));
+    }
+    capitalApi.getAllTransactions(uid, authToken, f, [startDate, endDate]);
+}
+
+// Returns net spending (deposits - expenses) across date range. Use Date.parse() for start/end dates
+// Ex. healthApi.getNetSpending(1234, abc, Date.parse(Date(2015, 1, 1, 0, 0, 0)), Date.now(), cb);
+function getNetSpending(uid, authToken, startDate, endDate, cb) {
+    var f = function(err, transactions) {
+        var total = 0.0;
+        _.each(transactions, function(el) {
+            var amount = el.amount;
+            total += amount;
+        });
+        cb(err, total);
+    }
+    capitalApi.getAllTransactions(uid, authToken, f, [startDate, endDate]);
+}
 module.exports = {
     getDebtBalanceRatio: getDebtBalanceRatio,
     getTotalDebt: getTotalDebt,
     getTotalBalance: getTotalBalance,
     getNetWorth: getNetWorth,
-    getDeposits: getDeposits
+    getDeposits: getDeposits,
+    getExpenses: getExpenses,
+    getNetSpending: getNetSpending
 }
